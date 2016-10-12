8122cd86747117783960eed9b9d7065b018fc457
Trond Mjøen <trond@twined.net>
2016-10-12 14:13:55 +0200

    Add package meta data

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule BrandoVillain.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   # Configuration for the OTP application
@@ -17,15 +19,22 @@ defmodule BrandoVillain.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    Package for including Villain in Brando projects through NPM.
+    """
+  end
+
+  defp package do
+    [name: :brando_villain,
+     files: [
+       "lib", "priv", "mix.exs", "package.json", "README*"],
+     maintainers: ["Twined Networks"],
+     licenses: [""],
+     links: %{"GitHub" => "https://github.com/twined/brando_villain"}
+    ]
+  end
+
   defp deps do
     []
   end
